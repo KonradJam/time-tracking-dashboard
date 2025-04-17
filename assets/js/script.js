f0b3fc207396a85b24c0894fca5038db2b4dacc3
KonradJam <acidburn.pl@gmail.com>
2025-04-17 22:11:07 +0200

    feat: Implement time tracking dashboard UI and logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
+const CONFIG = {
+    apiEndpoinds: {
+        data: '/assets/data.json'
+    }
+}
 
 const timeActivity = document.querySelectorAll('.time__activity');
+const btnUserTimeframes = document.querySelectorAll('.time__user-timeframe');
 
 const fetchData = async () => {
     try {
-        const response = await fetch('../../assets/data.json');
+        const response = await fetch(CONFIG.apiEndpoinds.data);
         if (!response.ok) {
             switch (response.status) {
                 case 404:
@@ -17,37 +23,55 @@ const fetchData = async () => {
         const data = await response.json();
         return data;
     } catch(error) {
-        console.error('Error fetching data:', error);
-        return null;
+        throw new Error('Error fetching data:', error);
     }
 }
 
-// const placeActivityData = (data, timeframe) => {
-//     timeActivity.forEach(activity => {
-//         if (activity.querySelector('.time__activity-type').textContent === data.title) {
-//             activity.querySelector('.time__activity-hours').textContent = data.timeframes[timeframe].current + 'hrs';
-//             switch (timeframe) {
-//                 case 'daily':
-//                     activity.querySelector('.time__activity-previous').textContent = 'Yesterday - ' + data.timeframes[timeframe].previous + 'hrs';
-//                     break;
-//                 case 'weekly':
-//                     activity.querySelector('.time__activity-previous').textContent = 'Last Week - ' + data.timeframes[timeframe].previous + 'hrs';
-//                     break;
-//                 case 'monthly':
-//                     activity.querySelector('.time__activity-previous').textContent = 'Last Month - ' + data.timeframes[timeframe].previous + 'hrs';
-//                     break;
-//                 default:
-//                     activity.querySelector('.time__activity-previous').textContent = 'Yesterday - ' + data.timeframes[timeframe].previous + 'hrs';
-//                     break;
-//             }
-//         }
-//     });
-// };
+const placeActivityData = (data, timeframe) => {
+    data.forEach((activity, index) => {
+        timeActivity[index].classList.add(`time__activity--${activity.title.toLowerCase().replace(' ', '-')}`);
+        const activityType = timeActivity[index].querySelector('.time__activity-type');
+        const activityHours = timeActivity[index].querySelector('.time__activity-hours');
+        const activityPrevious = timeActivity[index].querySelector('.time__activity-previous');
+        const activityMenu = timeActivity[index].querySelector('.time__activity-menu');
+        
+        activityType.textContent = activity.title;
+        activityHours.textContent = activity.timeframes[timeframe].current + 'hrs';
+        switch (timeframe) {
+            case 'daily':
+                activityPrevious.textContent = `Yesterday - ${activity.timeframes[timeframe].previous}hrs`;
+                break;
+            case 'weekly':
+                activityPrevious.textContent = `Last Week - ${activity.timeframes[timeframe].previous}hrs`;
+                break;
+            case 'monthly':
+                activityPrevious.textContent = `Last Month - ${activity.timeframes[timeframe].previous}hrs`;
+                break;
+            default:
+                activityPrevious.textContent = `Yesterday - ${activity.timeframes[timeframe].previous}hrs`;
+                break;
+        }
+        activityMenu.classList.remove('time__activity-menu--hidden');
+    });
+};
 
-// const initialize = async () => {
-//     const data = await fetchData();
-//     if (!data) return;
-//     placeActivityData(data, 'daily');
-// }
+const initialize = async () => {
+    const data = await fetchData();
 
-// initialize();
+    if (!data) throw new Error('No data found');
+
+    placeActivityData(data, 'daily');
+    document.querySelector('button[data-timeframe="daily"]').classList.add('time__user-timeframe--active');
+
+    btnUserTimeframes.forEach(btn => {
+        btn.addEventListener('click', () => {
+            btnUserTimeframes.forEach(btn => {
+                btn.classList.remove('time__user-timeframe--active');
+            });
+            placeActivityData(data, btn.dataset.timeframe);
+            btn.classList.add('time__user-timeframe--active');
+        });
+    });
+}
+
+initialize();
